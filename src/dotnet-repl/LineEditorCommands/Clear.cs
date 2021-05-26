6146f395821b47da5dccab6c7ee6c07c7ed9517c
Jon Sequeira <jonsequeira@gmail.com>
2021-05-25 20:39:58 -0700

    remove license headers

--- src/dotnet-repl/LineEditorCommands/Clear.cs
+++ src/dotnet-repl/LineEditorCommands/Clear.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using RadLine;
+﻿using RadLine;
 
 namespace dotnet_repl.LineEditorCommands
 {
