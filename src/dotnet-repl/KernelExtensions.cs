62510a6900eed2d3318aabd6614e931eb1916bb7
Jon Sequeira <jonsequeira@gmail.com>
2025-03-10 12:23:48 -0700

    fix extension loading, add support for secret management

--- src/dotnet-repl/KernelExtensions.cs
+++ src/dotnet-repl/KernelExtensions.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.CommandLine;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Threading;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.Commands;
 using Microsoft.DotNet.Interactive.CSharp;
 using Microsoft.DotNet.Interactive.Directives;
+using Microsoft.DotNet.Interactive.Events;
 using Microsoft.DotNet.Interactive.Formatting;
 using Microsoft.DotNet.Interactive.FSharp;
 using Microsoft.DotNet.Interactive.PackageManagement;
+using Microsoft.DotNet.Interactive.PowerShell;
 using Recipes;
 using Spectre.Console;
 
@@ -60,7 +65,7 @@ internal static class KernelExtensions
 
             var processId = process.Id;
 
-            context.Display($"Attach your debugger to process {processId} ({process.ProcessName}).");
+            KernelInvocationContextExtensions.Display(context, $"Attach your debugger to process {processId} ({process.ProcessName}).");
 
             while (!Debugger.IsAttached)
             {
@@ -180,6 +185,135 @@ internal static class KernelExtensions
         });
 
         Formatter.Register(formatter);
+
+        return kernel;
+    }
+
+    public static CompositeKernel UseNuGetExtensions(
+        this CompositeKernel kernel)
+    {
+        var packagesToCheckForExtensions = new ConcurrentQueue<PackageAdded>();
+
+        kernel.AddMiddleware(async (command, context, next) =>
+        {
+            await next(command, context);
+
+            while (packagesToCheckForExtensions.TryDequeue(out var packageAdded))
+            {
+                var packageRootDir = packageAdded.PackageReference.PackageRoot;
+
+                var extensionDir =
+                    new DirectoryInfo
+                    (Path.Combine(
+                         packageRootDir,
+                         "interactive-extensions",
+                         "dotnet"));
+
+                if (extensionDir.Exists)
+                {
+                  
+                    await LoadExtensionsFromDirectoryAsync(
+                        kernel,
+                        extensionDir,
+                        context);
+                }
+            }
+        });
+
+        kernel.RegisterForDisposal(
+            kernel.KernelEvents
+                  .OfType<PackageAdded>()
+                  .Where(pa => pa?.PackageReference.PackageRoot is not null)
+                  .Distinct(pa => pa.PackageReference.PackageRoot)
+                  .Subscribe(added => packagesToCheckForExtensions.Enqueue(added)));
+
+        return kernel;
+    }
+
+    public static async Task LoadExtensionsFromDirectoryAsync(
+        this CompositeKernel kernel,
+        DirectoryInfo extensionDir,
+        KernelInvocationContext context)
+    {
+        await new PackageDirectoryExtensionLoader().LoadFromDirectoryAsync(
+            extensionDir,
+            kernel,
+            context);
+    }
+
+    internal static bool CanBeInstantiated(this Type type)
+    {
+        return type is { IsAbstract: false, IsGenericTypeDefinition: false, IsInterface: false };
+    }
+
+    public static CompositeKernel UseSecretManager(
+        this CompositeKernel kernel,
+        SecretManager secretManager)
+    {
+        if (secretManager is null)
+        {
+            throw new ArgumentNullException(nameof(secretManager));
+        }
+
+        kernel.AddMiddleware(async (command, context, next) =>
+        {
+            if (command is not RequestInput { SaveAs: { } saveAs } requestInput)
+            {
+                await next(command, context);
+                return;
+            }
+
+            if (secretManager.TryGetValue(requestInput.SaveAs, out var value))
+            {
+                context.Publish(new InputProduced(value, requestInput));
+
+                var message =
+                    $"""
+                     Using previously saved value for `{requestInput.SaveAs}`.
+
+                     {MoreInfoMessage()}
+                     """;
+                context.Publish(new DisplayedValueProduced(
+                                    message,
+                                    requestInput,
+                                    [new FormattedValue("text/markdown", message)]));
+            }
+            else
+            {
+                using var _ = context.KernelEvents.Subscribe(@event =>
+                {
+                    if (@event is InputProduced inputProduced &&
+                        inputProduced.Command.GetOrCreateToken() == requestInput.GetOrCreateToken())
+                    {
+                        secretManager.SetValue(requestInput.SaveAs, inputProduced.Value);
+
+                        var message =
+                            $"""
+                             Your response for value `{saveAs}` has been saved and will be reused without a prompt in the future. 
+
+                             {MoreInfoMessage()}
+                             """;
+                        context.Publish(new DisplayedValueProduced(
+                                            message,
+                                            requestInput,
+                                            [new FormattedValue("text/markdown", message)]));
+                    }
+                });
+
+                await next(command, context);
+            }
+
+            string MoreInfoMessage() =>
+                $"""
+                 > 💡 To remove this value from your [SecretStore](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.secretstore/?view=ps-modules), run the following command in a PowerShell cell:
+                 > 
+                 > ```powershell
+                 >     Remove-Secret -Name "{requestInput.SaveAs}" -Vault {secretManager.VaultName}
+                 > ```
+
+                 > 📝 For more information, see [SecretManagement](https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/overview?view=ps-modules).
+                 """;
+        });
 
         return kernel;
     }
